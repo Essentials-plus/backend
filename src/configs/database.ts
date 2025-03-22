e6020dd1604167bf8b71a564dbdd41f6c76e06bd
rayhan926 <rayhanbd7075@gmail.com>
2025-03-22 20:38:39 +0600

    fix: update default pagination limit to 20 in database configuration

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -20,7 +20,7 @@ export const prisma = new PrismaClient()
   .$extends(
     pagination({
       pages: {
-        limit: 5, // set default limit to 20
+        limit: 20, // set default limit to 20
         includePageCount: true, // include counters by default
       },
     }),
